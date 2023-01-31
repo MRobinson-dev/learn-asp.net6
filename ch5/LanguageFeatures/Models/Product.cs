824d1ebaa9ab7da56c01b2dda43f37ea8927ce31
Marie Robinson <mariethe3rd.dev@gmail.com>
2023-01-31 08:47:28 -0600

    page 77 complete

---
+++ ch5/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace LanguageFeatures.Models;
+
+public class Product
+{
+    public string? Name { get; set; }
+    public decimal? Price { get; set; }
+    public static Product[] GetProducts()
+    {
+        Product kayak = new Product { Name = "Kayak",Price = 275m };
+        Product lifeJacket = new Product { Name = "Lifejacket", Price = 48.95m };
+
+        return new Product[] { kayak,lifeJacket, null };
+    }
+}
